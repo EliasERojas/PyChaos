052c7940e08603ce64819eb2374d6cba2732b8b7
elias <rojas.elias@outlook.com>
2024-01-03 08:48:02 -0300

    infalible (?

--- sudoku/sudoku-bueno.py
+++ sudoku/sudoku-bueno.py
@@ -1,7 +1,33 @@
-import time
-import copy
+from copy import deepcopy
+from time import time
+from collections import deque
 from random import choice,randint,shuffle
 from typing import Tuple,List
+
+sudoku6 = [
+    [0, 0, 0, 0, 0, 0, 0, 1, 3],
+    [0, 4, 0, 0, 0, 0, 0, 8, 0],
+    [2, 0, 0, 0, 6, 0, 0, 0, 0],
+    [9, 0, 6, 0, 0, 0, 4, 0, 0],
+    [0, 0, 0, 8, 0, 0, 0, 0, 0],
+    [0, 0, 0, 3, 0, 0, 0, 0, 0],
+    [0, 3, 0, 1, 0, 0, 5, 0, 0],
+    [0, 0, 0, 0, 4, 0, 7, 0, 6],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0]
+]
+
+sudoku0 = [
+    [1, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0]
+]
+
 sudoku_puzzle = [
     [5, 3, 0, 0, 7, 0, 0, 0, 0],
     [6, 0, 0, 1, 9, 5, 0, 0, 0],
@@ -38,29 +64,38 @@ sudoku_puzzle3 = [
     [0, 0, 0, 2, 0, 0, 6, 0, 0],
 ]
 
+sudoku7 = [
+    [0, 0, 0, 0, 0, 0, 4, 0, 8],
+    [0, 0, 1, 0, 8, 0, 0, 0, 0],
+    [0, 0, 0, 6, 0, 0, 9, 7, 0],
+    [5, 0, 8, 9, 0, 1, 0, 0, 0],
+    [3, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 5, 2, 8, 0, 3, 0],
+    [8, 0, 7, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 4, 5, 0, 0],
+    [0, 6, 2, 8, 9, 0, 0, 0, 0]
+]
+
 
 COUNT = 2
 SIZE = 9
+THIRD_SIZE = 3
 EMPTY = 0
+maskval = (511,510,509,507,503,495,479,447,383,255)
+bitval = (0,1,2,4,8,16,32,64,128,256)
 
-def count_possibilities(possibilities_list : List[bool]) :
+def count_bits(n) :
     count = 0
-    for i in range (SIZE) :
-        if possibilities_list[i] : 
-            count += 1
+    while n :
+        count += n & 1
+        n >>= 1
     return count
 
-
 class SudokuSolver : 
-    def __init__(self, sudoku : List[List[int]] = None):
-        self.possibilities = None
-        self.solution = None
-        if sudoku :
-            self.new_board(sudoku)
-
-    def new_board(self, sudoku : List[List[int]]) :
-        self.possibilities = [[[ True for i in range(SIZE) ] for j in range(SIZE) ] for k in range(SIZE)]
+    def __init__(self, sudoku : List[List[int]] | None = None):
+        self.possibilities = [[511 for j in range(SIZE) ] for k in range(SIZE)]
         self.solution = [[ EMPTY for i in range(SIZE) ] for j in range(SIZE) ]
+        self.placed = 0
         for i in range(SIZE) : 
             for j in range(SIZE) : 
                 cell_value = sudoku[i][j]
@@ -69,104 +104,135 @@ class SudokuSolver :
 
     def place_number(self, x : int, y : int, val : int) :
         self.solution[x][y] = val
+        self.placed += 1
+        self.possibilities[x][y] = 0
+        mask = maskval[val]
         for i in range(SIZE) : 
-            self.possibilities[x][i][val - 1] = False
-            self.possibilities[i][y][val - 1] = False
+            self.possibilities[x][i] &= mask
+            self.possibilities[i][y] &= mask
 
-        for i in range (SIZE//3) :
-            for j in range(SIZE//3) :
-                self.possibilities[x//3 * 3 + i][y//3 * 3 + j][val - 1] = False
-    
-    def can_place_number(self, x : int, y : int, val : int) -> bool:
-        return self.solution[x][y] == EMPTY and self.possibilities[x][y][val - 1]
+        for i in range (THIRD_SIZE) :
+            for j in range(THIRD_SIZE) :
+                self.possibilities[x//3 * 3 + i][y//3 * 3 + j] &= mask
 
-    def cell_with_least_possibilities(self) -> Tuple [int,int,int] | None:
-        max = 9
-        cell = None
-        for i in range (SIZE) :
-            for j in range (SIZE) :
-                c = count_possibilities(self.possibilities[i][j])
-                if self.solution[i][j] == EMPTY and 0 < c and c <= max :
-                    max = c
-                    cell = (i,j)
-        return cell
+    def place_single_possibility(self) :
+        for val in range(1,10) :
+            bit = bitval[val]
 
-    def is_solved(self) :
-        for i in range (SIZE) :
-            for j in range(SIZE) :
-                if count_possibilities(self.possibilities[i][j]) > 0 :
-                    return False
-        return True
+            for i in range (SIZE) :
+                col = -1
+                for j in range(SIZE) :
+                    if self.possibilities[i][j] & bit > 0 :
+                        if col < 0:
+                            col = j
+                        else : 
+                            col = -1
+                            break
+                if col >= 0 :
+                    self.place_number(i, col, val)
+
+            for i in range (SIZE) :
+                row = -1 
+                for j in range(SIZE) :
+                    if self.possibilities[j][i] & bit > 0 :
+                        if row < 0:
+                            row = j
+                        else :
+                            row = -1
+                            break
+                if row >= 0:
+                    self.place_number(row, i, val)
+
+    def boxes_single_possibility(self) :
+        for i in range(0,SIZE,THIRD_SIZE) :
+            for j in range(0,SIZE,THIRD_SIZE) :
+                for val in range(1,10) :
+                    pair = self.box_single_possibility(i,j,val)
+                    if pair[0] >= 0 :
+                        self.place_number(pair[0],pair[1],val)
+
+    def box_single_possibility(self, x, y, val): 
+        bit = bitval[val]
+        row = -1
+        col = -1
+        for i in range(THIRD_SIZE) :
+            for j in range(THIRD_SIZE) :
+                if (self.possibilities[x + i][y + j] & bit) > 0 :
+                    if row < 0 :
+                        row = x + i
+                        col = y + j
+                    else :
+                        row = -1
+                        return (row,col)
+        return (row,col)
 
     def solve(self) :
-        if self.is_solved() :
-            return self.solution
-        cell = self.cell_with_least_possibilities()
-        if not cell : 
+        before = 0
+        after = 1
+        while (after - before) > 0 : 
+            before = self.placed
+            self.trivial_moves()
+            self.boxes_single_possibility()
+            self.place_single_possibility()
+            after = self.placed
+
+        if self.placed < 81 :
+            if self.brute_force() : 
+                return self.solution
             return None
-        x, y = cell
-        for i in range (1,10) :
-            if self.can_place_number(x,y,i) :
-                sol = self.solution.copy()
-                pos = self.possibilities.copy()
-                self.place_number(x,y,i)
-                self.trivial_moves()
-                if self.solve() != None :
-                    return self.solution
-                self.solution = sol
-                self.possibilities = pos
-        return None
+
+        return self.solution
+
+    def brute_force(self) :
+        x, y = self.least_possibilities_cell()
+        for val in range (1,10) :
+            bit = bitval[val]
+            if self.possibilities[x][y] & bit > 0 :
+                start = time()
+                sol_copy = deepcopy(self.solution)
+                pos_copy = deepcopy(self.possibilities)
+                placed_copy = self.placed
+                self.place_number(x,y,val)
+                self.solve()
+                if self.placed == 81 :
+                    return True
+                self.possibilities = pos_copy
+                self.solution = sol_copy
+                self.placed = placed_copy
+        return False
+
+    def least_possibilities_cell(self) :
+        x = 0
+        y = 0
+        min = SIZE
+        for i in range(SIZE) :
+            for j in range(SIZE) :
+                c = count_bits(self.possibilities[i][j])
+                if c and c < min :
+                    min = c
+                    x = i
+                    y = j
+        return (x,y)
+                
 
     def trivial_moves(self) :
-        changed = True
-        while changed : 
-            changed = False
-            for i in range(SIZE) :
-                for j in range(SIZE) :
-                    if self.is_trivial(i,j) :
-                        changed = True
-                        val = self.possibilities[i][j].index(True)
-                        self.place_number(i,j,val + 1)
+        for i in range(SIZE) :
+            for j in range(SIZE) :
+                if count_bits(self.possibilities[i][j]) == 1 :
+                    changed = True
+                    self.set_trivial(i,j)
 
-    def is_trivial(self, x : int, y : int) :
-        return count_possibilities(self.possibilities[x][y]) == 1 and not self.solution[x][y]
+    def set_trivial(self, x, y) :
+        p = self.possibilities[x][y] 
+        num = 0
+        while p :
+            p >>= 1
+            num += 1
+        self.place_number(x, y, num)
 
-def random_filled_sudoku() -> List[List[int]] :
-    generated_sudoku = [ [0 for i in range(SIZE)] for j in range (SIZE) ] 
-    cells = [ (i,j) for i in range(SIZE) for j in range (SIZE) ] 
-    solver = SudokuSolver()
-    while cells :
-        cell = choice(cells)
-        x, y = cell
-        generated_sudoku[x][y] = randint(1,9)
-        solver.new_board(generated_sudoku)
-        if solver.solve() : 
-            cells.pop(cells.index(cell))
-        else : 
-            generated_sudoku[x][y] = EMPTY
-    return generated_sudoku
-
-def random_sudoku(sudoku, level) :
-    cells = [ (i,j) for i in range(SIZE) for j in range (SIZE) ] 
-    shuffle(cells)
-    solver = SudokuSolver()
-    count = 0
-    while cells and level > 0:
-        level -= 1
-        x, y = cells.pop()
-        prev = sudoku[x][y]
-        sudoku[x][y] = EMPTY
-        solver.new_board(sudoku)
-        if not solver.solve() :
-            level += 1
-            sudoku[x][y] = prev
-    return sudoku
-
-def count_filled(list) :
-    count = 0 
-    for i in range (SIZE) :
-        for j in range (SIZE) :
-            if list[i][j] != EMPTY :
-                count += 1
-    return count
-
+start = time()
+solver = SudokuSolver(sudoku_puzzle3)
+solver.solve()
+print(solver.solution)
+end = time()
+print(end - start)
--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+print(pedro)
 x , y = (1,3)
 if y == 3 :
     x, y = (4,5)
